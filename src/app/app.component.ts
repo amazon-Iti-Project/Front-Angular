b3b00308104ec571111be718c42d44da764fad39
kamal7241 <kamal.korney@gmail.com>
2021-01-22 05:52:03 +0200

    Modules +db json + app Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+enum Icons {
+  coffe = 0,
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss',
+  ]
 })
+
 export class AppComponent {
   title = 'AmazonProject';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './appComponent/header/header.component';
+import { FooterComponent } from './appComponent/footer/footer.component';
+import { CarouselComponent } from './appComponent/carousel/carousel.component';
+import { SidebarComponent } from './appComponent/sidebar/sidebar.component';
+import { HomeComponent } from './appComponent/home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CarouselComponent,
+    SidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+     //font awesome Module
+     // use this command to init
+     //(npm install --save font-awesome angular-font-awesome)
+    //  AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
