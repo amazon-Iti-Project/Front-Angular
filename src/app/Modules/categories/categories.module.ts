81690ce32b2244fa38245b42c4ca48437bea2cd7
pc <pc@DESKTOP-32MIV93>
2021-01-22 17:20:06 +0200

    nav

--- src/app/Modules/categories/categories.module.ts
+++ src/app/Modules/categories/categories.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductsDetalisComponent } from './products-detalis/products-detalis.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductsDetalisComponent],
   imports: [
     CommonModule
   ]
 })
+
 export class CategoriesModule { }
---
+++ src/app/Modules/categories/products-detalis/products-detalis.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductsDetalisComponent } from './products-detalis.component';
+
+describe('ProductsDetalisComponent', () => {
+  let component: ProductsDetalisComponent;
+  let fixture: ComponentFixture<ProductsDetalisComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProductsDetalisComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductsDetalisComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
