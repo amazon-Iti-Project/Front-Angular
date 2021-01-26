56deb0043ba87ef67c00bf72e73f4073b3809a68
kamal7241 <kamal.korney@gmail.com>
2021-01-26 17:56:48 +0200

    update in seller

--- src/app/Modules/seller/sellerComponents/seller-add-product/seller-add-product.component.ts
+++ src/app/Modules/seller/sellerComponents/seller-add-product/seller-add-product.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { Ibrand } from 'src/app/viewModel/Ibrand';
+import { BrandService } from './../../../../services/brand/brand.service';
 
 @Component({
   selector: 'app-seller-add-product',
@@ -13,11 +15,11 @@ import { Router } from '@angular/router';
 })
 export class SellerAddProductComponent implements OnInit {
   imgName: string | undefined
-  url: string | ArrayBuffer | undefined
   prodForm: FormGroup = this.fb.group({})
   subscription: Subscription[] = [];
   categories: Icategory[] = [];
-  constructor(private fb: FormBuilder, private catServ: CategoryService, private proServ: ProductService
+  brands: Ibrand[] = [];
+  constructor(private fb: FormBuilder, private catServ: CategoryService,private brandSer:BrandService, private proServ: ProductService
     , private route: Router) { }
 
   ngOnInit(): void {
@@ -26,20 +28,64 @@ export class SellerAddProductComponent implements OnInit {
     {
       this.categories = res
     },err=>console.log(err))
+       // get brands
+       this.brandSer.getAllBrands().subscribe((res)=>
+       {
+         this.brands = res
+       },err=>console.log(err))
+
+    // get brands
     this.prodForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(5)]],
-      price: [0, [Validators.required, Validators.min(0)]],
-      quantity: [1, [Validators.required, Validators.min(1)]],
+      price: [, [Validators.required, Validators.min(1)]],
+      quantity: [, [Validators.required, Validators.min(1)]],
       category: ['', Validators.required],
       brand: [, Validators.required],
       discount: [0, Validators.required],
       shipping: [0, Validators.required],
       size: ['', Validators.required],
-      colors: [[], Validators.required],
+      color: ['', Validators.required],
       image: [, Validators.required],
-      tags: [[], Validators.required]
+      tags: [[], ]
     })
+    console.log(this.prodForm.controls['image'].value)
 
   }
+
+        // selectImg by input
+ onSeleImg(e:any):void{
+    var file = e.target.files[0];
+    console.log(file.name)
+    // to change form control name value
+    
+    this.readImageFromFile(file)
+}
+// read image From  File object and then Upload image on it
+ readImageFromFile(file:File) {
+  // Check if the file is an image.
+  console.log(file.type.indexOf("image"));
+  if (file.type && file.type.indexOf('image') === -1) {
+    // console.log('File is not an image.', file.type, file);
+    return;
+  }
+  this.imgName = file.name;
+  // this event is fired when readAsDataUrl content is read
+  const reader = new FileReader();
+  reader.onload=(e) => {
+    console.log("event: ",e)
+    // file local url
+    let url = e.target?.result;
+    // to add to formgroup control when added an img
+    this.prodForm.controls['image'].setValue(url);
+   
+    
+  };
+  // return file or a blob
+  reader.readAsDataURL(file);
+}
+
+addProduct():void{
+  console.log(this.prodForm.value)
+}
   
 }
