2919ebf223625b1fd0b85de15af8d9a7faa74cea
pc <pc@DESKTOP-32MIV93>
2021-01-26 18:00:51 +0200

    product Commit

--- src/app/Modules/categories/products-detalis/products-detalis.component.ts
+++ src/app/Modules/categories/products-detalis/products-detalis.component.ts
@@ -1,15 +1,41 @@
+import { ProductService } from './../../../services/product/product.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Iproduct } from 'src/app/viewModel/IProduct';
+import{Location} from '@angular/common'
 
 @Component({
   selector: 'app-products-detalis',
   templateUrl: './products-detalis.component.html',
   styleUrls: ['./products-detalis.component.scss']
 })
+
+
 export class ProductsDetalisComponent implements OnInit {
+  prd:Iproduct|null=null;
+  prdID:number=0;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private pService:ProductService,
+    private location: Location) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+      this.activatedRoute.paramMap.subscribe((params:ParamMap)=>{
+        let prdIDParam = params.get('1')
+        this.prdID= (prdIDParam)? parseInt(prdIDParam) : 0;
+        // this.prd= this.prdService.getProductByID(this.prdID);
+        this.pService.getProductById(this.prdID).subscribe(
+          (res)=>{
+            this.prd=res;
+          }
+        )
+
+
+      })
+
+     console.log(this.prdID);
+     console.log(this.prd);
+    }
 
 }
--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -2,7 +2,7 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Iproduct } from './../../viewModel/Iproduct';
+import { Iproduct } from './../../viewModel/IProduct'
 
 @Injectable({
   providedIn: 'root'
